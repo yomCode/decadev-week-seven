a1382dc5f4cff3987f11730c4fdd2f7adfec480f
Yomi <mustaphaabayomi71@gmail.com>
2022-11-09 22:41:40 +0100

    commit'

---
+++ src/main/java/dev/decagon/fcaebookdemo/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package dev.decagon.fcaebookdemo.services.serviceImpl;
+
+
+import dev.decagon.fcaebookdemo.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+}
